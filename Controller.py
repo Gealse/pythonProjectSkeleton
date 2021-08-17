889cf80ee9bdf073addfb551647afc33e6ecb0c5
Gealse <57640411+Gealse@users.noreply.github.com>
2021-08-17 17:49:10 +0200

    created WebController file

--- Controller.py
+++ Controller.py
@@ -1,8 +1,18 @@
+from WebController import WebController
+
 
 class Controller(object):
 
+    webController = None
+
     def __init__(self):
+        self.webController = WebController()
+
+    def setCredentials(self):
+        pass
+
+    def getCredential(self):
         pass
 
     def mainController(self):
-        input('Hello World')
+        pass
---
+++ WebController.py
@@ -0,0 +1,5 @@
+
+class WebController(object):
+
+    def __init__(self):
+        pass
