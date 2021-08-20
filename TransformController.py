a4b5989eec76de8059c818e6d4703f8f45351635
Gealse <57640411+Gealse@users.noreply.github.com>
2021-08-20 12:32:26 +0200

    dataclass

--- TransformController.py
+++ TransformController.py
@@ -1,5 +1,28 @@
+from dataclasses import dataclass
 
 class TransformController(object):
 
+    itemFactory : None
+    
     def __init__(self):
-        pass
+        itemFactory = ItemFactory()
+
+class ItemFactory(object):
+
+    itemsList : list
+
+    def __init__(self):
+        self.itemsList.append(Item(name = 'hello'))
+        input(itemsList)
+        input(itemsList[0])
+
+@dataclass
+class Item(object):
+
+    name : str
+    
+    def setName(self,name):
+        self.name = name
+
+    def getName(self):
+        return self.name
